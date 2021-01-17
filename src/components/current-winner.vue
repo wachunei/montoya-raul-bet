c3a7a240338b117134d7e27bf17ef4b324c5c3e9
Javier G. Montoya S <jgmontoya@pm.me>
2021-01-17 12:01:10 -0300

    feat(current-winner): add current winner card with floating head

---
+++ src/components/current-winner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="border-2 rounded flex w-max p-2 shadow-lg">
+    <p class="font-bold">Current winner</p>
+    <img v-if="threshold" src="../assets/javier-head.png" />
+    <img v-else src="../assets/raul-head.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    threshold: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
